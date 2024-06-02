9a7dbffaec0d252fcf2d64c9dc8ac721e6090171
vfffgrttgh <163544061+vfffgrttgh@users.noreply.github.com>
2024-06-02 18:05:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// JAVASCRIPT PROGRAM
+
+console.log("JavaScript is my fav language!");
+
+const Vid = document.getElementById("Video");
+Vid.onclick = () => {
+    confirm("You clicked me!");
+    console.log("Hello Users!");
+    console.warn("User Interacted with us.");
+}
+const Vid2 = document.getElementById("Video-2");
+Vid2.onclick = () => {
+    confirm("You clicked me!");
+    console.log("Hello Users!");
+    console.warn("User Interacted with us.");
+}
